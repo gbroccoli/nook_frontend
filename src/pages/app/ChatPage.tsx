7ed59152752b675444841b349b1be0e3a4010e0d
gbroccoli <vovo.r@ya.ru>
2026-02-25 12:29:38 +1000

    edit

--- src/pages/app/ChatPage.tsx
+++ src/pages/app/ChatPage.tsx
@@ -860,6 +860,7 @@ export function ChatPage() {
       dynacast: true,
       videoCaptureDefaults: buildVideoCaptureOptions(mediaSettings),
       audioOutput: mediaSettings.audioOutputId ? { deviceId: mediaSettings.audioOutputId } : undefined,
+      webAudioMix: true,
     })
     liveKitRoomRef.current = roomInstance
     try {
